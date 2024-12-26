76765dcd20d6dbe981ca2a08620f548e43f44892
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-12-26 18:41:12 +0100

    day 4

--- 2024/day_4/script.mjs
+++ 2024/day_4/script.mjs
@@ -43,3 +43,122 @@
 // Take a look at the little Elf's word search. How many times does XMAS appear?
 
 import fs from "fs";
+
+const readFile = (path) => {
+  const file = fs.readFileSync(path, "utf-8");
+  const splittedFile = file.split("\n");
+  const arrayDivision = [];
+  for (let i = 0; i < splittedFile.length; i++) {
+    const element = splittedFile[i].replace(/(\r\n|\n|\r)/gm, "").split("");
+    arrayDivision.push(element);
+  }
+
+  return arrayDivision;
+};
+
+const splittedFile = readFile("./data.txt");
+// console.log(splittedFile);
+
+let total = 0;
+const findHorizontalXmas = () => {
+  for (let row of splittedFile) {
+    for (let j = 0; j <= row.length - 4; j++) {
+      if (row[j] === "X" && row[j + 1] === "M" && row[j + 2] === "A" && row[j + 3] === "S") {
+        total++;
+      }
+    }
+    for (let j = 3; j < row.length; j++) {
+      if (row[j] === "X" && row[j - 1] === "M" && row[j - 2] === "A" && row[j - 3] === "S") {
+        total++;
+      }
+    }
+  }
+};
+
+const findVerticalXmas = () => {
+  for (let i = 0; i < splittedFile.length; i++) {
+    const currentArray = splittedFile[i];
+    for (let j = 0; j < currentArray.length; j++) {
+      const element = currentArray[j];
+      if (element === "X") {
+        //se si trova nelle prime tre righe, possiamo controllare solo verso il basso, altrimenti possiamo controllare anche verso l'alto
+        if (
+          i >= 3 &&
+          splittedFile[i - 1][j] === "M" &&
+          splittedFile[i - 2][j] === "A" &&
+          splittedFile[i - 3][j] === "S"
+        ) {
+          total++;
+        }
+        if (
+          i <= splittedFile.length - 4 &&
+          splittedFile[i + 1][j] === "M" &&
+          splittedFile[i + 2][j] === "A" &&
+          splittedFile[i + 3][j] === "S"
+        ) {
+          total++;
+        }
+      }
+    }
+  }
+};
+
+const findDiagonalXmas = () => {
+  for (let i = 0; i < splittedFile.length; i++) {
+    const currentArray = splittedFile[i];
+    for (let j = 0; j < currentArray.length; j++) {
+      const element = currentArray[j];
+      // se ci trovamo  ma minore di currentArray - 4 possiamo cercare a destra
+      // se ci troviamo a j>3 possiamo cercare a sinistra
+      if (element === "X") {
+        if (
+          j >= 3 &&
+          i <= splittedFile.length - 4 &&
+          splittedFile[i + 1][j - 1] === "M" &&
+          splittedFile[i + 2][j - 2] === "A" &&
+          splittedFile[i + 3][j - 3] === "S"
+        ) {
+          total++;
+        }
+        if (
+          j >= 3 &&
+          i >= 3 &&
+          splittedFile[i - 1][j - 1] === "M" &&
+          splittedFile[i - 2][j - 2] === "A" &&
+          splittedFile[i - 3][j - 3] === "S"
+        ) {
+          total++;
+        }
+        if (
+          j <= currentArray.length - 4 &&
+          i <= splittedFile.length - 4 &&
+          splittedFile[i + 1][j + 1] === "M" &&
+          splittedFile[i + 2][j + 2] === "A" &&
+          splittedFile[i + 3][j + 3] === "S"
+        ) {
+          total++;
+        }
+        if (
+          j <= currentArray.length - 4 &&
+          i >= 3 &&
+          splittedFile[i - 1][j + 1] === "M" &&
+          splittedFile[i - 2][j + 2] === "A" &&
+          splittedFile[i - 3][j + 3] === "S"
+        ) {
+          total++;
+        }
+      }
+    }
+  }
+};
+
+const findAllXmas = () => {
+  findHorizontalXmas();
+  findVerticalXmas();
+  findDiagonalXmas();
+  console.log(total);
+};
+
+console.time("execution time");
+findAllXmas();
+console.timeEnd("execution time");
